247944bbe84950905c3b0a8e7ccc0a5b2c8687db
ddiazsouto <ddiazsouto@gmail.com>
2023-03-17 14:36:12 +0000

    Working prototype, cleaned and refactored code, working in the last methos: access_the_data. Which needs major cleaning. Login method also needs some tidying up

--- scrap_users.py
+++ scrap_users.py
@@ -1,5 +1,5 @@
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from scraper import Scraper
 import time
 from login_info import EMAIL, PASSWD
@@ -8,18 +8,18 @@ from login_info import EMAIL, PASSWD
 databricks_target_path = "https://adb-909870082157824.4.azuredatabricks.net/"\
                          "?o=909870082157824#notebook/908271178432007/command/908271178432009"
 
-logs = {}
+logs = {'Inactive clusters': [], 'Active clusters': []}
 
 
 class DatabricksUsers(Scraper):
 
     def __init__(self, headless):
         super().__init__(headless)
-        self.clusters_path = []
+        self.found_clusters = {}
 
     def login(self, databricks_target_path):
         self._driver.get(databricks_target_path)
-        sign_in_button = self._await_element_located(By.XPATH, "//uses-legacy-bootstrap/div/div/div/div/div[1]", 10)
+        sign_in_button = self._await_element_located(By.XPATH, "//uses-legacy-bootstrap/div/div/div/div/div[1]", 5)
         sign_in_button.click()
         try:
             email_box = self._await_element_located(By.NAME, "loginfmt", 5).send_keys(EMAIL)
@@ -34,56 +34,60 @@ class DatabricksUsers(Scraper):
             print("We're in I would say")
 
     def collect_existing_clusters_path(self):
-        open_compute_tab = self._driver.find_element(By.XPATH, "//a[@data-testid='Compute']")
-        open_compute_tab.click()
+        open_compute_tab = self._driver.find_element(By.XPATH, "//a[@data-testid='Compute']").click()
         try:
             for table_row in range(2, 10):
-                cluster = self._await_element_located(By.XPATH, "//div[1]/div[3]/div[1]/div[2]/div[1]/div[1]/div[1]"
+                cluster_row = self._await_element_located(By.XPATH, "//div[1]/div[3]/div[1]/div[2]/div[1]/div[1]/div[1]"
                                                                 "/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]"
                                                                 f"/div[1]/div[2]/table[1]/tbody[1]/tr[{table_row}]", 5)
-                tag_content = cluster.get_attribute('innerHTML')
-                cluster_relative_path = tag_content[tag_content.find("<a href="):]
-                cluster_relative_path = cluster_relative_path[:cluster_relative_path.find(">")]
-                self.clusters_path.append(self._driver.current_url + cluster_relative_path[26:])
+                cluster_name, cluster_path = self._extract_cluster_name_and_path(cluster_row)
+                self.found_clusters[cluster_name] = cluster_path
         except TimeoutException:
-            print("Finished at:", table_row)
-        logs['found_clusters'] = table_row
-        breakpoint()
+            pass # The table of clusters does not contain more rows
 
     def lookup_active_clusters_notebooks(self):
-        for cluster_path in self.clusters_path:
+        for cluster_name, cluster_path in self.found_clusters.items():
             self._driver.get(cluster_path)
-            
-
-    def _next_window(self):
-        new_window = self._driver.window_handles[1]
-        self._driver.close()
-        time.sleep(20)
-        self._driver.switch_to.window(new_window)
+            try:
+                scraper._await_element_located(By.XPATH, "//span[normalize-space()='Terminate']", 5)
+                logs['Active clusters'].append(cluster_name)
+                try:
+                    self.access_the_data()
+                except TimeoutException:
+                    continue
+            except TimeoutException:
+                logs['Inactive clusters'].append(cluster_name)
 
     def access_the_data(self):
-        open_cluster_dropdown = self._await_element_located(By.XPATH,
-                                                            "//button[@data-testid='cluster-dropdown-v2-button']", 15)
-        open_cluster_dropdown.click()
-
-        select_cluster_name = self._await_element_located(By.XPATH, "//span[@data-testid='attached-cluster-name']", 15)
-        select_cluster_name.click()
-        access_spark_UI = self._await_element_located(By.XPATH, "//div[normalize-space()='Spark UI']", 15)
-        access_spark_UI.click()
-        # print(access_spark_UI.get_attribute('innerHTML'))
-
-        self._next_window()
+        # Clicks on the cluster Notebooks tab
         open_notebooks_tab = self._await_element_located(By.XPATH, "/html[1]/body[1]/div[1]/div[3]/div[1]/div[2]/div[1]/div[1]/div[1]/form[1]/div[2]/uses-legacy-bootstrap[1]/ul[1]/li[2]/a[1]", 10)
         open_notebooks_tab.click()
+        # Not we iterate through the pages reading the information
+        last_page = False
+        while not last_page:
+            read_table_with_usernames = self._await_element_located(By.XPATH, "//tbody[@class='du-bois-light-table-tbody']", 5)
+            self.clusters_information = read_table_with_usernames.text
+            try:
+                print("HERE")
+                next_page = self._await_element_located(By.XPATH, "//span[@aria-label='right']//*[name()='svg']", 5)
+                print("1")
+                breakpoint()
+                next_page.click()
+            except NoSuchElementException:
+                print("10")
+                last_page = True
+                breakpoint()
+        print("2")
+        breakpoint()
 
-        read_table_with_usernames = self._driver.find_element(By.XPATH, "//tbody[@class='du-bois-light-table-tbody']")
+    def _extract_cluster_name_and_path(self, cluster_row):
+        cluster_name = cluster_row.text.split("\n")[0]
+        tag_content = cluster_row.get_attribute('innerHTML')
+        cluster_relative_path = tag_content[tag_content.find("<a href="):]
+        cluster_relative_path = cluster_relative_path[:cluster_relative_path.find(">")]
+        cluster_absolute_path = self._driver.current_url + cluster_relative_path[26:]        
 
-        page_2_rel_path = self._driver.find_element(By.XPATH, "//a[normalize-space()='2']")
-        next_page = self._driver.find_element(By.XPATH, "//span[@aria-label='right']//*[name()='svg']")
-
-        notebook_ui_information = read_table_with_usernames.text
-
-
+        return cluster_name, cluster_absolute_path
 
     def quit():
         self._driver.quit()
@@ -91,11 +95,14 @@ class DatabricksUsers(Scraper):
 
 
 scraper = DatabricksUsers(True)
+print("\n\n>>> Login\n")
 scraper.login(databricks_target_path)
+print("\n\n>>> Collect clusters path\n")
 scraper.collect_existing_clusters_path()
-scraper.quit()
-
+print("\n\n>>> Look at the active clusters\n")
+scraper.lookup_active_clusters_notebooks()
 breakpoint()
+scraper.quit()
 
 
 
